f412e98c1841d536f720a415a9215c12260409d7
laurachica-ctrl <laura.chica@alegra.com>
2026-04-21 17:49:06 -0500

    Update hubspot.js

--- netlify/functions/hubspot.js
+++ netlify/functions/hubspot.js
@@ -33,28 +33,3 @@ exports.handler = async function (event) {
     if (body) {
       options.headers["Content-Length"] = Buffer.byteLength(body);
     }
-
-    const req = https.request(options, (res) => {
-      let data = "";
-      res.on("data", (chunk) => { data += chunk; });
-      res.on("end", () => {
-        resolve({
-          statusCode: res.statusCode,
-          headers: { "Access-Control-Allow-Origin": "*", "Content-Type": "application/json" },
-          body: data,
-        });
-      });
-    });
-
-    req.on("error", (err) => {
-      resolve({
-        statusCode: 500,
-        headers: { "Access-Control-Allow-Origin": "*" },
-        body: JSON.stringify({ error: err.message }),
-      });
-    });
-
-    if (body) req.write(body);
-    req.end();
-  });
-};
